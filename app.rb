e607da051a9795265cae2be92da415823ce31b41
Adam Patterson <fakeempire@gmail.com>
2010-05-30 13:14:03 +0800

    add simple ordering via GET string

--- app.rb
+++ app.rb
@@ -30,7 +30,12 @@ end
 get '/:db/:table' do
   db = Sequel.connect(:adapter=>ADAPTER, :host=>HOST, :database=>params[:db], :user=>USER, :password=>PASSWORD)
   table = params[:table].to_sym
-  @row_list = db[table].limit(20)
+  @path = '/' + params[:db].to_s + '/' + params[:table].to_s
+  qs = db[table].limit(20)
+  if params[:o]
+    qs = qs.order(params[:o].to_sym)
+  end
+  @row_list = qs
   @schema_list = db.schema(table)
   haml :table_detail
 end
