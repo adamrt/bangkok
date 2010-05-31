e929a508c65c17d88cdf7820bf0bb8e061153988
Adam Patterson <fakeempire@gmail.com>
2010-06-01 01:36:43 +0800

    add bare minimum tests

---
+++ tests.rb
@@ -0,0 +1,34 @@
+require 'app'
+require 'rubygems'
+require 'test/unit'
+require 'rack/test'
+
+set :environment, :test
+
+class BangkokdbTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def text_db_list
+    get '/db'
+    assert last_response.ok?
+  end
+  
+  def test_table_list
+    get '/db/vurbmoto'
+    assert last_response.ok?
+  end
+
+  def test_table_content
+    get '/db/vurbmoto/articles_article/content'
+    assert last_response.ok?
+  end
+
+  def test_ordering
+    get '/db/vurbmoto/articles_article/content', :o => 'title'
+    assert last_response.ok?
+  end
+end
